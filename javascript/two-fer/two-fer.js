bb6b62852dff99cb72a931cf162e562ecaf5e4bf
Arlys sanjuanelo <arlissanjuanelo1@gmail.com>
2021-05-27 16:09:52 -0500

    se suben ejercicios de clases

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const twoFer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const twoFer = (nombre ='you') => {
+
+  return `One for ${nombre}, one for me.`;
+
+
 };
