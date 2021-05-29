dca9495cc75fe24d56192bbdb7930868c8735d76
Arlys sanjuanelo <arlissanjuanelo1@gmail.com>
2021-05-29 10:41:47 -0500

    se sube otro ejercicio

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,40 @@
+
+export class Matrix {
+  constructor(string) {
+    this._rows = this.splitInRows(string)
+    this._columns = this.splitInColumns(string)
+    }
+
+  get rows(matrix) {
+  
+    return matrix.split('\n').map( entry => entry.split(' ').map(entry => parseInt(entry, 10)))
+
+  }
+
+  get columns(matrix) {
+    
+    const firstLineNumber = new RegExp(/^([0-9]+)/gm) 
+    const leadingWhitespace = new RegExp(/^[ \t]+/gm)
+    const columns = []
+
+    let matrixToWorkOn = matrix
+
+    while (matrixToWorkOn.trim().length) {
+      const column = matrixToWorkOn.match(firstLineNumber).map(entry => parseInt(entry, 10))
+      columns.push([... column])
+
+      matrixToWorkOn = matrixToWorkOn.replace(firstLineNumber, '').replace(leadingWhitespace, '')
+    }
+   return columns
+  }
+
+  get rows() {
+    return this._rows
+  }
+
+  get columns() {
+    return this._columns
+  }
+
+  }
+
